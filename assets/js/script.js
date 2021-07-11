7b14d6f8ea6043fdabd6ff3cc591dd53313211bc
hicey <hiceycook@gmail.com>
2021-07-11 19:33:34 -0400

    form improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,33 @@ var tasksToDoEl = document.querySelector("#tasks-to-do")
 // ADD TASK BUTTON CLICK FUNCTION
 var createTaskHandler = function (event) {
     event.preventDefault();
+    // COLLECT FORM VALUES //
+    var taskNameInput = document.querySelector("input[name = 'task-name']").value;
+    var taskTypeInput = document.querySelector("select[name = 'task-type']").value;
+
+    // CREATE A NEW LI //
     var listItemEl = document.createElement("li");
+
+    // GIVE CLASS NAME TO STYLE LIST ITEM //
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // CREATE A DIV TO HOLD TASK INFO AND ADD TO LI //
+    var taskInfoEl = document.createElement("div");
+
+    // GIVE ITEM A CLASS NAME //
+    taskInfoEl.className = "task-info";
+
+    // ADD HTML CONTENT TO DIV //
+    taskInfoEl.innerHTML = "<h3 class ='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    // ADD NEW DIV TO LI //
+    listItemEl.appendChild(taskInfoEl);
+
+    // ADD THE NEW LI ELEMENT TO THE UL //
     tasksToDoEl.appendChild(listItemEl);
-}
-// ADD TASK BUTTON EVENT
+
+};
+// ADD THE BUTTON EVENT
 formEl.addEventListener("submit", createTaskHandler);
 
 
