7be894cc8b35b3288cf64eb586f19d3b9472447e
hicey <hiceycook@gmail.com>
2021-07-11 18:55:48 -0400

    event form instead of button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
-var buttonEl = document.querySelector("#save-task-button");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do")
 
 // ADD TASK BUTTON CLICK FUNCTION
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 // ADD TASK BUTTON EVENT
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
 
